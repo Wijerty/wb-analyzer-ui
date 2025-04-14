5aa0c3d1238dbf512a63078c80bf3a3ed8700014
Oniss <69907542+Wijerty@users.noreply.github.com>
2025-04-14 21:49:56 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, 
   theme: {
     extend: {
       colors: {
-        'wb-purple': '#cb11ab', // Основной цвет Wildberries
-        'wb-purple-dark': '#a80d8e', // Темный оттенок основного цвета
-        'wb-purple-light': '#e030c3', // Светлый оттенок основного цвета
-        'wb-gray': '#f6f6f6', // Фоновый цвет Wildberries
-        'wb-text': '#333333', // Цвет текста Wildberries
-        'wb-success': '#2aad27', // Зеленый цвет для успешных действий
-        'wb-warning': '#ffa900', // Желтый цвет для предупреждений
-        'wb-error': '#ff3347', // Красный цвет для ошибок
+        'wb-purple': '#cb11ab', 
+        'wb-purple-dark': '#a80d8e', 
+        'wb-purple-light': '#e030c3', 
+        'wb-gray': '#f6f6f6', 
+        'wb-text': '#333333', 
+        'wb-success': '#2aad27', 
+        'wb-warning': '#ffa900', 
+        'wb-error': '#ff3347',
       },
       fontFamily: {
         sans: ['Segoe UI', 'Roboto', 'Arial', 'sans-serif'],
